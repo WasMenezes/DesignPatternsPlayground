9c156b40b3bc5ae0b9f0774709ff84ac45054b9b
Was Menezes <wasmenezes@MacBook-Pro-de-Was.local>
2024-09-14 15:38:17 -0300

    refactor loan abstract factory structure

--- abstract_factory/loan_example/loan.go
+++ abstract_factory/loan_example/loan.go
@@ -1,25 +1,57 @@
 package loan_example
 
-import "errors"
+import (
+	"errors"
+)
 
 type Loan interface {
-	Rate() float64
+	GetLoanId() string
+	GetAmount() float64
+	GetIncome() float64
+	GetInstallments() int
+	GetLoanType() string
+	GetRate() float64
 }
 
-type LoanDetails struct {
+type MortgageLoan struct {
 	loanId       string
 	amount       float64
 	income       float64
 	installments int
 	loanType     string
+	rate         float64
 }
 
-type MortgageLoan struct {
-	*LoanDetails
-	rate float64
+func (m *MortgageLoan) GetLoanId() string    { return m.loanId }
+func (m *MortgageLoan) GetAmount() float64   { return m.amount }
+func (m *MortgageLoan) GetIncome() float64   { return m.income }
+func (m *MortgageLoan) GetInstallments() int { return m.installments }
+func (m *MortgageLoan) GetLoanType() string  { return m.loanType }
+func (m *MortgageLoan) GetRate() float64     { return m.rate }
+
+type CarLoan struct {
+	loanId       string
+	amount       float64
+	income       float64
+	installments int
+	loanType     string
+	rate         float64
 }
 
-func NewMortgageLoan(id string, amount, income float64, installments int) (*MortgageLoan, error) {
+func (c *CarLoan) GetLoanId() string    { return c.loanId }
+func (c *CarLoan) GetAmount() float64   { return c.amount }
+func (c *CarLoan) GetIncome() float64   { return c.income }
+func (c *CarLoan) GetInstallments() int { return c.installments }
+func (c *CarLoan) GetLoanType() string  { return c.loanType }
+func (c *CarLoan) GetRate() float64     { return c.rate }
+
+type LoanFactory interface {
+	CreateLoan(id string, amount, income float64, installments int) (Loan, error)
+}
+
+type MortgageLoanFactory struct{}
+
+func (f *MortgageLoanFactory) CreateLoan(id string, amount, income float64, installments int) (Loan, error) {
 	if installments > 420 {
 		return nil, errors.New("the maximum number of installments for mortgage loan is 420")
 	}
@@ -27,11 +59,30 @@ func NewMortgageLoan(id string, amount, income float64, installments int) (*Mort
 		return nil, errors.New("the maximum number of installments could not exceed 25% of monthly income")
 	}
 	return &MortgageLoan{
-		&LoanDetails{id, amount, income, installments, "Mortgage"},
-		10,
+		loanId:       id,
+		amount:       amount,
+		income:       income,
+		installments: installments,
+		loanType:     "Mortgage",
+		rate:         10.0,
 	}, nil
 }
 
-func (m *MortgageLoan) Rate() float64 {
-	return m.rate
+type CarLoanFactory struct{}
+
+func (f *CarLoanFactory) CreateLoan(id string, amount, income float64, installments int) (Loan, error) {
+	if installments > 60 {
+		return nil, errors.New("the maximum number of installments for car loan is 60")
+	}
+	if income*0.3 < amount/float64(installments) {
+		return nil, errors.New("the maximum number of installments could not exceed 30% of monthly income")
+	}
+	return &CarLoan{
+		loanId:       id,
+		amount:       amount,
+		income:       income,
+		installments: installments,
+		loanType:     "Car",
+		rate:         10.0,
+	}, nil
 }
--- abstract_factory/loan_example/loan_test.go
+++ abstract_factory/loan_example/loan_test.go
@@ -6,22 +6,39 @@ import (
 )
 
 func TestLoanFactory_ShouldCreateAMortgageLoan(t *testing.T) {
-	loan, err := NewMortgageLoan("abcd-1234", 100000, 10000, 240)
-	assert.Equal(t, loan.LoanDetails.loanId, "abcd-1234")
-	assert.Equal(t, loan.LoanDetails.amount, float64(100000))
-	assert.Equal(t, loan.LoanDetails.income, float64(10000))
-	assert.Equal(t, loan.LoanDetails.installments, 240)
+	loanFactory := &MortgageLoanFactory{}
+	loan, err := loanFactory.CreateLoan("abcd-1234", 100000, 10000, 240)
 	assert.Nil(t, err)
+	assert.Equal(t, loan.GetLoanId(), "abcd-1234")
+	assert.Equal(t, loan.GetAmount(), float64(100000))
+	assert.Equal(t, loan.GetIncome(), float64(10000))
+	assert.Equal(t, loan.GetInstallments(), 240)
 }
 
-func TestLoanFactory_ShouldNotCreateAMortgageLoanWithInstallmentsGreaterThan420(t *testing.T) {
-	loan, err := NewMortgageLoan("abcd-1234", 100000, 10000, 450)
+func TestLoanFactory_ShouldNotCreateAMortgageLoanWithInstallmentsGreaterThan420Months(t *testing.T) {
+	loanFactory := &MortgageLoanFactory{}
+	loan, err := loanFactory.CreateLoan("abcd-1234", 100000, 10000, 450)
 	assert.EqualError(t, err, "the maximum number of installments for mortgage loan is 420")
 	assert.Nil(t, loan)
 }
 
 func TestLoanFactory_ShouldNotCreateAMortgageIfTheInstallmentOccupiesMoreThan25PercentOfTheMonthlyIncome(t *testing.T) {
-	loan, err := NewMortgageLoan("abcd-1234", 200000, 1000, 240)
+	loanFactory := &MortgageLoanFactory{}
+	loan, err := loanFactory.CreateLoan("abcd-1234", 200000, 1000, 240)
 	assert.EqualError(t, err, "the maximum number of installments could not exceed 25% of monthly income")
+	assert.Nil(t, loan)
+}
+
+func TestLoanFactory_ShouldNotCreateACarLoanWithInstallmentsGreaterThan60Months(t *testing.T) {
+	loanFactory := &CarLoanFactory{}
+	loan, err := loanFactory.CreateLoan("abcd-1234", 100000, 10000, 61)
+	assert.EqualError(t, err, "the maximum number of installments for car loan is 60")
+	assert.Nil(t, loan)
+}
+
+func TestLoanFactory_ShouldNotCreateACarIfTheInstallmentOccupiesMoreThan30PercentOfTheMonthlyIncome(t *testing.T) {
+	loanFactory := &CarLoanFactory{}
+	loan, err := loanFactory.CreateLoan("abcd-1234", 20000, 1000, 12)
+	assert.EqualError(t, err, "the maximum number of installments could not exceed 30% of monthly income")
 	assert.Nil(t, loan)
 }
